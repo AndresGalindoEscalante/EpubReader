a25ab17e8c744f00959632844b516703d0345c10
Andres <andresgalindoescalante@gmail.com>
2025-10-28 20:22:51 +0100

    Agregar archivos de proyecto.

---
+++ EpubReader/Controller/FileReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace EpubReader.Controller
+{
+    internal class FileReader
+    {
+        public static void UnzipFile(string filename)
+        {
+            try
+            {
+                string zipPath = @".\Epubs\[gotrek & felix 01] - trollslayer_-, william king.epub";
+                //string extractPath = @".\extract";
+
+                //ZipFile.CreateFromDirectory(filename, zipPath);
+
+                var a = ZipFile.OpenRead(zipPath);
+
+                //ZipFile.ExtractToDirectory(zipPath, extractPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ EpubReader/Form1.cs
@@ -0,0 +1,35 @@
+﻿using EpubReader.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EpubReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string fileName =@".\start\";
+            string zipPath = @".\result.zip";
+            FileReader.UnzipFile(fileName);
+        }
+
+        private void AddBookButton(object sender, EventArgs e)
+        {
+            string fileName = @".\start\";
+            string zipPath = @".\result.zip";
+            FileReader.UnzipFile(fileName);
+        }
+    }
+}
